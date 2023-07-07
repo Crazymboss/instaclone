b7e1506df4a8165454c17f5775c9270d111477b6
Daniel Purda <purdaioandaniel@gmail.com>
2023-07-08 00:57:07 +0300

    final version

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,24 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    current_user.like!(@post)
+    if params[:comment_id]
+      @post = Post.find(params[:post_id])
+      @comment = Comment.find(params[:comment_id])
+      current_user.like!(@comment) if @comment
+    elsif params[:post_id]
+      @post = Post.find(params[:post_id])
+      current_user.like!(@post) if @post
+    end
   end
+  
 
   def destroy
-    @post = Post.find_by(id: params[:post_id])
-    current_user.likes.where(likeable_id: @post.id, likeable_type: @post.class.name).delete_all
-  end
+    if params[:comment_id]
+      @post = Post.find_by(id: params[:post_id])
+      @comment = Comment.find_by(params[:comment_id])
+      current_user.likes.where(likeable_id: @comment.id, likeable_type: @comment.class.name).delete_all
+    elsif params[:post_id]
+        @post = Post.find_by(id: params[:post_id])
+        current_user.likes.where(likeable_id: @post.id, likeable_type: @post.class.name).delete_all
+    end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:create, :new] do
     resources :comments, only: [:create, :new] do
-      resources :likes, only: :create
+      resources :likes, only: [:create, :destroy]
     end
     resources :likes, only: :create do
       collection do
